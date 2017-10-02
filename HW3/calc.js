d0ee27f22bce1ffada8f3c2b9f636bd6775326ee
luonglearnstocode <luong.nguyen@metropolia.fi>
2017-10-02 18:12:53 +0300

    group html elements to class in html file

--- HW3/calc.js
+++ HW3/calc.js
@@ -1,3 +1,12 @@
 /*
  * Implement all your JavaScript in this file!
  */
+ var stack = '';
+
+$(function() {
+    $(".digit").click(function() {
+    	stack += $(this).text();
+    	console.log($(this).text());
+    	console.log(stack);
+ 	});
+});
