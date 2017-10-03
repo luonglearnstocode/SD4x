ecbb561ac486fcdef4204fdce4c2e776ea983947
luonglearnstocode <luong.nguyen@metropolia.fi>
2017-10-03 19:32:46 +0300

    case 1 - performing an operation on 2 numbers

--- HW3/calc.js
+++ HW3/calc.js
@@ -1,12 +1,44 @@
 /*
  * Implement all your JavaScript in this file!
  */
- var stack = '';
+var stack = [];
+var display = '';
+var functionMap = {
+	"addButton" : (a, b) => a + b,
+	"subtractButton" : (a, b) => a - b,
+	"multiplyButton" : (a, b) => a * b,
+	"divideButton" : (a, b) => a == 0 ? 'Infinity' : a / b
+};
 
 $(function() {
     $(".digit").click(function() {
-    	stack += $(this).text();
-    	console.log($(this).text());
-    	console.log(stack);
+    	display += $(this).val();
+    	$("#display").val(display);
+    	console.log(display);
  	});
+
+ 	$("#clearButton").click(function() {
+ 		display = '';
+ 		stack = [];
+ 		$("#display").val('');
+ 	});
+
+ 	$(".operator").click(function() {
+ 		stack.push(Number(display));
+ 		stack.push(this.id);
+ 		display = ''; 		
+ 	});
+
+ 	$("#equalsButton").click(function() {
+ 		stack.push(Number(display));
+ 		console.log(stack);
+ 		display = evaluate(stack);
+ 		$("#display").val(display);
+ 	});
+
+ 	function evaluate(stack) {
+ 		return functionMap[stack[1]](stack[0], stack[2]);
+ 	}
+
+
 });
