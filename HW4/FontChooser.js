ab933225f77fc4ccc2e2b47cdd4f118538a7fd10
luonglearnstocode <luong.nguyen@metropolia.fi>
2017-10-07 01:19:01 +0300

    changing the font size

--- HW4/FontChooser.js
+++ HW4/FontChooser.js
@@ -19,18 +19,38 @@ class FontChooser extends React.Component {
 	handleChangeCheckbox() {
 		this.setState({isBold : !this.state.isBold});
 	}
+
+	decreaseFontSize() {
+		if (this.state.size > this.props.min) {
+			this.setState({size : this.state.size - 1});	
+		}
+	}
 	
+	increaseFontSize() {
+		if (this.state.size < this.props.max) {
+			this.setState({size : this.state.size + 1});	
+		}
+	}
+
+	resetFontSize() {
+		this.setState({size : parseInt(this.props.size)});
+	}
   
 	render() {
 		var weight = this.state.isBold ? 'bold' : 'normal';
-		var inlineStyles = {fontSize: this.state.size, fontWeight : weight};	
+		var color = (this.state.size == this.props.max || this.state.size == this.props.min) ? 'red' : 'black';
+		var inlineStyles = {fontSize: this.state.size, fontWeight : weight, color : color};	
 
 		return (
 			<div>
-				<input class="form" type="checkbox" id="boldCheckbox" hidden='true' defaultChecked={this.state.isBold} onChange={this.handleChangeCheckbox.bind(this)}/>
-				<button class="form" id="decreaseButton" hidden='true'>-</button>
-				<span class="form" id="fontSizeSpan" hidden='true'>{this.props.size}</span>
-				<button class="form" id="increaseButton" hidden='true'>+</button>
+				<input class="form" type="checkbox" id="boldCheckbox" hidden='true' 
+					defaultChecked={this.state.isBold} onChange={this.handleChangeCheckbox.bind(this)}/>
+				<button class="form" id="decreaseButton" hidden='true' 
+					onClick={this.decreaseFontSize.bind(this)}>-</button>
+				<span class="form" id="fontSizeSpan" hidden='true' 
+					onDoubleClick={this.resetFontSize.bind(this)}>{this.state.size}</span>
+				<button class="form" id="increaseButton" hidden='true' 
+					onClick={this.increaseFontSize.bind(this)}>+</button>
 				<span id="textSpan" style={inlineStyles} onClick={this.toggle.bind(this)}>{this.props.text}</span>
 			</div>
 		);
