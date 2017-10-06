0ecf2b758af016bfb4de119b9ac48965bdd5531f
luonglearnstocode <luong.nguyen@metropolia.fi>
2017-10-06 22:19:48 +0300

    initial rendering

--- HW4/FontChooser.js
+++ HW4/FontChooser.js
@@ -1,21 +1,29 @@
 class FontChooser extends React.Component {
-
-    constructor(props) {
-	super(props);
-    }
-    
-
-    render() {
-
-	return(
-	       <div>
-	       <input type="checkbox" id="boldCheckbox" hidden='true'/>
-	       <button id="decreaseButton" hidden='true'>-</button>
-	       <span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
-	       <button id="increaseButton" hidden='true'>+</button>
-	       <span id="textSpan">{this.props.text}</span>
-	       </div>
-	);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {isBold : this.props.bold == 'true', 
+								 size : parseInt(this.props.size)};
+		
+	}
+		
+	
+  
+	render() {
+		var weight = this.state.isBold ? 'bold' : 'normal';
+		var inlineStyles = {fontSize: this.state.size, fontWeight : weight};	
+		
+		return (
+			<div>
+				<input type="checkbox" id="boldCheckbox" hidden/>
+				<button id="decreaseButton" hidden='true'>-</button>
+				<span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
+				<button id="increaseButton" hidden='true'>+</button>
+				<span id="textSpan" style={inlineStyles}>{this.props.text}</span>
+				
+				// <span>{typeof this.state.size}</span>
+				// <span>{weight}</span>
+			</div>
+		);
+	}
 }
 
