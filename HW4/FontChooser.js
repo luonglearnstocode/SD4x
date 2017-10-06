963e3f765e361e451d21b0dd9511a7a1fbbe786e
luonglearnstocode <luong.nguyen@metropolia.fi>
2017-10-07 00:57:05 +0300

    add checkbox functionality

--- HW4/FontChooser.js
+++ HW4/FontChooser.js
@@ -2,23 +2,22 @@ class FontChooser extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {isBold : this.props.bold == 'true',
-									isHidden : true,
-								 size : parseInt(this.props.size)};
+						isHidden : true,
+						size : parseInt(this.props.size)};
 		
 	}
 	
 	toggle() {
-		this.state.isHidden = ! this.state.isHidden;
+		this.setState({isHidden : !this.state.isHidden});
+
 		var formElements = document.getElementsByClassName("form");
-		if (this.state.isHidden) {
-			for (let element of formElements) {
-				element.setAttribute('hidden', true);
-			}
-		} else {
-			for (let element of formElements) {
-				element.removeAttribute('hidden');
-			}
+		for (let element of formElements) {
+			element.hidden = this.state.isHidden;
 		}
+	}
+
+	handleChangeCheckbox() {
+		this.setState({isBold : !this.state.isBold});
 	}
 	
   
@@ -28,14 +27,11 @@ class FontChooser extends React.Component {
 
 		return (
 			<div>
-				<input class="form" type="checkbox" id="boldCheckbox" hidden='true'/>
+				<input class="form" type="checkbox" id="boldCheckbox" hidden='true' defaultChecked={this.state.isBold} onChange={this.handleChangeCheckbox.bind(this)}/>
 				<button class="form" id="decreaseButton" hidden='true'>-</button>
 				<span class="form" id="fontSizeSpan" hidden='true'>{this.props.size}</span>
 				<button class="form" id="increaseButton" hidden='true'>+</button>
 				<span id="textSpan" style={inlineStyles} onClick={this.toggle.bind(this)}>{this.props.text}</span>
-				
-				<span>{typeof this.state.size}</span>
-				<span id='test'>aaa</span>
 			</div>
 		);
 	}
