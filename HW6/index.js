f265b49e004e9895da4b90a0e71709a51ca4f38f
luonglearnstocode <luong.nguyen@metropolia.fi>
2017-10-11 22:11:04 +0300

    add api findAnimals

--- HW6/index.js
+++ HW6/index.js
@@ -27,19 +27,30 @@ app.get('/findToy', (req, res) => {
 		});	  
 	} else {
         res.json({});
-    }
+    }  
+});
 
-	// Toy.findOne( query, (err, toy) => {
-	// 	if (err) {
-	// 	    res.type('html').status(500);
-	// 	    res.send('Error: ' + err);
-	// 	} else if (toy) {
-	// 		res.json(toy);
-	// 	}
-	// 	else {
-	// 	    res.json({});
-	// 	}
-	// });	  
+app.get('/findAnimals', (req, res) => {
+	var query = {};
+	if (req.query.species) {
+	    query.species = req.query.species;
+	}
+	if (req.query.trait) {
+	    query.traits = req.query.trait;
+	}
+	if (req.query.gender) {
+	    query.gender = req.query.gender;
+	}
+
+	Animal.find( query, '-_id name species breed gender age', (err, animals) => {
+		if (err) {
+		    res.type('html').status(500);
+		    res.send('Error: ' + err);
+		}
+		else {
+		    res.json(animals);
+		}
+	});
 
 });
 
